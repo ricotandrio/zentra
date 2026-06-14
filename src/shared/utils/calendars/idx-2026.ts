26b24b2dc017f9fd56428c19952341bb4659b801
ricotandrio <ricotandrio@gmail.com>
2026-06-14 14:26:46 +0700

    feat: check holiday day

---
+++ src/shared/utils/calendars/idx-2026.ts
@@ -0,0 +1,7 @@
+/**
+ * This file contains a set of IDX holidays for the year 2026.
+ * The holidays are represented as strings in the format 'YYYY-MM-DD'.
+ */
+export const IDX_HOLIDAYS_2026 = new Set([
+  '2026-01-01', // New Year's Day
+]);
---
+++ src/shared/utils/calendars/index.ts
@@ -0,0 +1,5 @@
+import { IDX_HOLIDAYS_2026 } from './idx-2026';
+
+export const IDX_HOLIDAYS: { [key: string]: Set<string> } = {
+  '2026': IDX_HOLIDAYS_2026,
+};
